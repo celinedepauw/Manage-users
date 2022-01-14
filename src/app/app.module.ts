ce9501141c0ed91bd0bfcba2b7c6931da9da157e
celin <celin@localhost>
2022-01-14 13:37:05 +0100

    add 4 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DetailsUserComponent } from './details-user/details-user.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    DetailsUserComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
