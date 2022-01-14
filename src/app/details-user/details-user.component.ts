fb30dd7a04d1702dca2ef40ce51b278227d959eb
celin <celin@localhost>
2022-01-14 16:31:50 +0100

    template details user component ok

--- src/app/details-user/details-user.component.ts
+++ src/app/details-user/details-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-details-user',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  goBackHome(){
+    this.router.navigateByUrl('/home')
+  }
 }
