f80dda98601475b3f4d556054eb72826809bdd7b
celin <celin@localhost>
2022-01-14 15:51:23 +0100

    template home component ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
@@ -38,6 +39,7 @@ import { CreateUserComponent } from './create-user/create-user.component';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCardModule,
     HttpClientModule,
     CommonModule
   ],
