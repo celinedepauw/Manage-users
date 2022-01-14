fecdda4127affbd16993567695bfbfe1124bf3a2
celin <celin@localhost>
2022-01-14 16:41:12 +0100

    template create user component ok

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-user',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  goBackHome(){
+    this.router.navigateByUrl('/home')
+  }
 }
