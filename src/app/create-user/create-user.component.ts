9d2e958932c10dd963277a6728e912622cfa7ce2
celin <celin@localhost>
2022-01-17 13:22:06 +0100

    request post to add a new user ok

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-create-user',
@@ -8,14 +11,52 @@ import { Router } from '@angular/router';
 })
 export class CreateUserComponent implements OnInit {
 
+  user!: User;
+  createForm!: FormGroup;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
+    this.createForm = new FormGroup({
+      lastname: new FormControl(''),
+      firstname: new FormControl(''),
+      email: new FormControl(''),
+      phoneNumber: new FormControl('')
+    })
+  }
+
+  createUser(){
+    this.user = {
+      firstName: this.createForm.value.firstname,
+      lastName: this.createForm.value.lastname,
+      email: this.createForm.value.email,
+      password: "mdp",
+      phoneNumber: this.createForm.value.phoneNumber 
+    }
+    this.userService.createNewUser(this.user)
+      .subscribe()
+
+    this.router.navigateByUrl('/home')
   }
 
   goBackHome(){
     this.router.navigateByUrl('/home')
   }
+
+  /*createUser(){
+    //console.log('soumission formulaire OK');
+    this.userService.createNewUser(
+      this.createForm.value.lastname,
+      this.createForm.value.firstname,
+      this.createForm.value.email,
+      "mot de passe",
+      this.createForm.value.phoneNumber 
+      )
+      .subscribe(user => console.log(user))
+
+    this.router.navigateByUrl('/home')
+  }*/
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from './user';
 import { Observable, of } from 'rxjs';
@@ -7,6 +7,10 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(
     private http: HttpClient
@@ -20,4 +24,30 @@ export class UserService {
   getUserById(id: string): Observable<User>{
     return this.http.get<User>(`http://localhost:5000/api/v1/users/${id}`)
   }
+
+  createNewUser(user: User): Observable<User>{
+    //console.log('user avant endpoint : ', user);
+    return this.http.post<User>(
+      'http://localhost:5000/api/v1/users',
+      user,
+     //facultatif :  this.httpOptions
+    )
+  }
+
+
+   /*createNewUser(lastName: string, firstName: string, email: string, password: string, phoneNumber: string): Observable<User>{
+    console.log(`user avant endpoint : nom=${lastName}, prénom=${firstName}, email=${email}, password=${password}, phoneNumber=${phoneNumber}`);
+    return this.http.post<User>(
+      'http://localhost:5000/api/v1/users',
+      {
+        firstName,
+        lastName,
+        email,
+        password,
+        phoneNumber
+      },
+      this.httpOptions
+    )
+  }*/
+
 }
